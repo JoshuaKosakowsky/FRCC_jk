b218b104599178a40dc73b95e13159a820f30c82
JoshuaKosakowsky <161373641+JoshuaKosakowsky@users.noreply.github.com>
2024-06-26 17:28:56 -0600

    Update SSADETL-Auto.py

--- python/SSADETL-Auto.py
+++ python/SSADETL-Auto.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
